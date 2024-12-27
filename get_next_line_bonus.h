c30e7bb44c2b553f58230e32ed918a61e99bb605
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2024-12-27 17:38:12 +0100

    - feat : add bonus part

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/26 19:22:32 by tsargsya          #+#    #+#             */
+/*   Updated: 2024/12/27 16:03:37 by tsargsya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stddef.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+# ifndef MAX_FD
+#  define MAX_FD 1024
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *line, char *readbuffer);
+void	ft_bzero(void *s, size_t n);
+char	*ft_strchr(const char *s, int c);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/26 19:22:28 by tsargsya          #+#    #+#             */
+/*   Updated: 2024/12/26 19:30:33 by tsargsya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+#include <stdlib.h>
+
+static size_t	ft_strlen(char *str);
+static void		*ft_memcpy(void *dest, const void *src, size_t n);
+
+char	*ft_strjoin(char *line, char *readbuffer)
+{
+	char	*str;
+	size_t	size;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!line || !readbuffer)
+		return (NULL);
+	j = ft_strlen(line);
+	while (readbuffer[i] && readbuffer[i] != '\n')
+		i++;
+	if (readbuffer[i] == '\n')
+		i++;
+	size = i + j;
+	str = malloc((size + 1) * sizeof(char));
+	if (!str)
+		return (free(line), NULL);
+	ft_memcpy(str, line, j);
+	i = 0;
+	while (readbuffer[i] && readbuffer[i] != '\n')
+		str[j++] = readbuffer[i++];
+	if (readbuffer[i] == '\n')
+		str[j++] = readbuffer[i++];
+	str[j] = '\0';
+	return (free(line), str);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)s;
+	while (n)
+	{
+		*ptr = 0;
+		ptr++;
+		n--;
+	}
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == 0)
+		return ((char *)s);
+	return (NULL);
+}
+
+static size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+static void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (dest == NULL && src == NULL)
+		return (dest);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
